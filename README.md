46bfbdeeecc69801015b7a23f7c635a691bf45c8
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-02-09 16:35:10 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,49 @@
+
+## A simple server to return signed S3 urls. 
+
+### Getting Started:
+
+* Update the file `s3_server.py` with the required details. 
+* Install dependencies : (pip install) boto, falcon, falcon_cors, gunicorn 
+* Run the server `gunicorn s3_server:api`
+
+
+
+Example Query using httpie :
+    
+```json
+ ~ http GET 'http://127.0.0.1:8000/api/S3Sign?objectName=log.txt'HTTP/1.1 200 OK
+Connection: close
+Date: Fri, 09 Feb 2018 15:29:30 GMT
+Server: gunicorn/19.7.1
+content-length: 162
+content-type: application/json; charset=UTF-8
+
+{
+    "signedUrl": "https://cog.sanger.ac.uk:443/tb15/uploads/log.txt?Signature=Sc1fSryIPlslX092eKVrZmXVjSU%3D&Expires=1518190470&AWSAccessKeyId=QNTED0B3JWYME5G1S56A"
+}
+```
+
+Test using Curl: (the server will print the guessed file type to pass as a header)
+
+```bash
+$ curl -H "Content-Type: text/plain" -T "log.txt" https://cog.sanger.ac.uk:443/tb15/uploads/log.txt\?Signature\=WTqc9VfhzDdwsho1nBkl4jxI1tg%3D\&Expires\=1518189029\&AWSAccessKeyId\=QNTED0B3JWYME5G1S56A
+```
+
+Example Use: 
+
+```javascript
+fetch("/api/S3Sign?objectName=" + fileName, {
+      method: "GET"
+    })
+      .then(
+        url => url.json(),
+        error => console.log("Error generating signed URL", error)
+      )
+      .then(url =>
+        fetch(url.signedUrl, {
+          method: "PUT",
+          body: file
+        })
+      )
+ ```
---
+++ s3_server.py
@@ -0,0 +1,54 @@
+import boto
+import boto.s3.connection
+import falcon
+from falcon_cors import CORS
+import mimetypes
+
+"""
+S3 Signed URL API. Will Guess filetype passed.
+
+Run with gunicorn: $ gunicorn s3_server:api
+
+"""
+
+access_key = '' #Fill in
+secret_key = '' #Fill in
+conn = boto.connect_s3(
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    host='cog.sanger.ac.uk',         # Fill in 
+    # is_secure=False,               # uncomment if you are not using ssl
+    calling_format=boto.s3.connection.OrdinaryCallingFormat(),
+)
+
+# Allow access via CORS requests.
+cors = CORS(allow_origins_list=['http://localhost:3000'], 
+            allow_headers_list=['Access-Control-Allow-Origin'])
+
+
+class signS3Upload(object):
+    def on_get(self, req, resp):
+        """Handles get requests"""
+        print("req data is: ", req.params)
+        resp.status = falcon.HTTP_200
+        resp.media = self.sign_s3_upload(req.params)
+
+    def sign_s3_upload(self, request):
+        object_name = request['objectName']
+        # Guess the MIME type to give to s3
+        content_type = mimetypes.guess_type(object_name)[0]
+        print("Guessed content type: ", content_type)
+
+        signed_url = conn.generate_url(
+            300,
+            "PUT",
+            'mybucket', #Bucket name
+            'uploads/' + object_name, #Bucket folder
+            headers={'Content-Type': content_type}
+        )
+
+        return {'signedUrl': signed_url}
+
+
+api = falcon.API(middleware=[cors.middleware])
+api.add_route('/api/S3Sign', signS3Upload())
